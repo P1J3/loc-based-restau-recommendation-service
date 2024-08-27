596d8832ee73879bf653cbfb978ef26602ed7ff1
Kim-Hyunhee <tta_bi@naver.com>
2024-08-27 23:17:15 +0900

    Chore: 주석 추가

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -21,14 +21,14 @@ export class LogInUserDto {
   @IsEmail()
   @ApiProperty({
     description: '이메일 주소',
-    example: 'dani@example.com',
+    example: 'test@example.com',
   })
   email: string;
 
   @IsNotEmpty()
   @ApiProperty({
     description: '비밀번호',
-    example: 'ekslek1954',
+    example: 'test1234',
   })
   password: string;
 }
