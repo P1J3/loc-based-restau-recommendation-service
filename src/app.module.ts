2d2d297fc3bd71a3e71ca9e0ac4a9e3ae43e9fc3
urimJ <ufrl128@dgu.ac.kr>
2024-08-27 18:26:06 +0900

    Init: 초기 세팅 - 디렉토리 구조, eslint, prettier

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PlaceModule } from './place/place.module';
+import { ReviewModule } from './review/review.module';
+
+@Module({
+  imports: [UserModule, PlaceModule, ReviewModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
