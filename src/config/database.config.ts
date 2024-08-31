23ea820b51f6af1642a9a033ef184b46ed7102de
jlee0505 <hj960322@gmail.com>
2024-08-31 18:59:50 +0900

    Fix: (평가) Postman POST 500 에러 해결

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { User } from 'src/modules/user/user.entity';
 import { Location } from 'src/modules/location/location.entity';
 import { Restaurant } from 'src/modules/place/place.entity';
+import { Review } from 'src/modules/review/review.entity';
 
 export const getTypeOrmConfig = (
   configService: ConfigService,
@@ -13,6 +14,6 @@ export const getTypeOrmConfig = (
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_NAME'),
-  entities: [User, Location, Restaurant],
+  entities: [User, Location, Restaurant, Review],
   synchronize: true, // 개발 환경에서만 true로 설정 (배포 환경에서는 false)
 });
