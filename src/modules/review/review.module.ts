a71f131d2131a0ef64788a55103e735f685c967e
jlee0505 <hj960322@gmail.com>
2024-08-31 17:14:30 +0900

    Feat: (평가) Module 작성

--- src/modules/review/review.module.ts
+++ src/modules/review/review.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
+import { Review } from './review.entity';
+import { Restaurant } from '../place/place.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Review, Restaurant])],
   controllers: [ReviewController],
   providers: [ReviewService],
 })
